70c2a72cab6a61e474190d3d9366e7774c8c5c61
davidangeli <davidangeli@users.noreply.github.com>
2020-02-17 13:18:16 +0100

    Kamisado board improvements -different tower shapes -skip button, in case -documentation, minor improvements

--- src/main/java/tictactoe/games/KamisadoPlane.java
+++ src/main/java/tictactoe/games/KamisadoPlane.java
@@ -8,20 +8,24 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import lombok.EqualsAndHashCode;
 import tictactoe.AI;
 import tictactoe.Main;
 import tictactoe.Player;
+import java.util.LinkedList;
 import java.util.Optional;
 
 /**
  * Graphical interface for the Kamisado Game. Extends javafx's GridPane.
- * Represents the game's table as a grid of buttons.
+ * Represents the game's table as a grid of buttons. Size is always 8x8.
  */
 public class KamisadoPlane extends GridPane {
     private final Kamisado game;
     private final Label whosturn;
     private final KamisadoButton[][] buttons = new KamisadoButton[8][8];
+    private final Button skipButton;
     private Kamisado.Tower activeTower = null;
 
     public KamisadoPlane(Kamisado game, Label whosturn, int width){
@@ -29,40 +33,47 @@ public class KamisadoPlane extends GridPane {
         setVgap(2);
         this.game = game;
         this.whosturn = whosturn;
+        //kamisado field buttons
         for (int i=0; i<8; i++){
             for (int j=0; j<8; j++) {
                 KamisadoButton button = new KamisadoButton(i, j, width / 10);
-                button.setBackground(new Background(new BackgroundFill(Kamisado.colorMap[i][j], null, null)));
-                button.drawTower();
                 buttons[i][j] = button;
                 this.add(button, j, i, 1, 1);
             }
         }
+
+        //skipbutton
+        skipButton = new Button("Skip");
+        skipButton.setOnAction(e -> skipPlaneStep());
+        this.add(skipButton, 7, 8, 1, 1);
+
         enableButtons();
     }
 
+    /**
+     * Controls which fields are enabled for the player, according to the game's actual standing.
+     */
     private void enableButtons(){
-        for (KamisadoButton[] arr: buttons) {
-            for (Button b : arr) {
-                b.setDisable(true);
-                b.setStyle("-fx-border-width: 1px;");
-            }
+        //disable all
+        this.getChildren().forEach(c -> c.setDisable(true));
+        if (game.getWinner().isPresent()) return;
 
-        }
-        //if (game.getWhosTurn() == Player.COMPUTER) {return;}
-
-        //enable towers
+        //enable towers if color matches last step, or first move
         for (Kamisado.Tower tower : game.getPlayerTowers()[Player.HUMAN.ordinal()]) {
             if (game.getSteps().isEmpty() || game.getSteps().getLast().getValue().isEmpty() || game.getSteps().getLast().getValue().get().getToFieldColor() == tower.color) {
                 buttons[tower.getI()][tower.getJ()].setDisable(false);
                 buttons[tower.getI()][tower.getJ()].setStyle("-fx-border-color: #f00000; -fx-border-width: 2px;");
             }}
 
-        //enable possible target fields
+        //enable possible target fields and skip button if there's no step
         if (activeTower != null) {
-            for (Kamisado.Step step : game.getNextSteps(activeTower)) {
+            LinkedList<Kamisado.Step> nextsteps = game.getNextSteps(activeTower);
+            for (Kamisado.Step step : nextsteps) {
                 buttons[step.toI][step.toJ].setDisable(false);
                 buttons[step.toI][step.toJ].setStyle("-fx-border-color: #f00000; -fx-border-width: 2px;");
+            }
+            if (nextsteps.isEmpty()) {
+                skipButton.setDisable(false);
             }
         }
     }
@@ -90,9 +101,8 @@ public class KamisadoPlane extends GridPane {
         whosturn.setText(game.getWhosTurn().toString());
     }
 
-
     /**
-     * This subclass of javafx's Button contains a preindexed TicTacToeGame.Step.
+     * This subclass of javafx's Button represents a field on the board.
      */
     @EqualsAndHashCode(callSuper = true)
     class KamisadoButton extends Button {
@@ -104,7 +114,9 @@ public class KamisadoPlane extends GridPane {
             this.j = j;
             this.width = width;
             this.setPrefSize(width,width);
+            this.setBackground(new Background(new BackgroundFill(Kamisado.colorMap[i][j], null, null)));
             super.setOnAction(this::click);
+            drawTower();
         }
 
         private void click (ActionEvent actionEvent) {
@@ -136,19 +148,24 @@ public class KamisadoPlane extends GridPane {
         }
 
         public void drawTower(){
-            if (game.getTable()[i][j] == null) {
-                //this.setText("");
+            Kamisado.Tower tower = game.getTable()[i][j];
+            if (tower == null) {
                 this.setGraphic(null);
+                return;
+            }
+
+            Shape towershape;
+            if (tower.player == Player.HUMAN) {
+                towershape = new Circle(width/5);
+                //((Circle)towershape).setRadius(width/5);
             }
             else {
-                //this.setText(Kamisado.colorMap[i][j].toString());
-                Circle circle = new Circle();
-                circle.setRadius(width/5);
-                circle.setFill(game.getTable()[i][j].getColor());
-                circle.setStroke(Color.BLACK);
-                circle.setStrokeWidth(2);
-                this.setGraphic(circle);
+                towershape = new Rectangle(width/2, width/2);
             }
+            towershape.setFill(game.getTable()[i][j].getColor());
+            towershape.setStroke(Color.BLACK);
+            towershape.setStrokeWidth(2);
+            this.setGraphic(towershape);
         }
     }
 }
